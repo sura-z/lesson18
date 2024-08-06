bd286d10e69eb4b5d18df536891c2f249e3b2e96
sura-z <169043346+sura-z@users.noreply.github.com>
2024-08-06 15:08:41 +0300

    Add files via upload

---
+++ module_4.1.py
@@ -0,0 +1,6 @@
+from fake_math import divide as fk_div
+from true_math import divide as tr_div
+print('result1 = ', fk_div(69, 3))
+print('result2 = ', fk_div(3, 0))
+print('result3 = ', tr_div(49, 7))
+print('result4 = ', tr_div(15, 0))
